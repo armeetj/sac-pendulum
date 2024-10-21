9464c2ea1487206cb4fc4261b0ea170d5fe7c2b0
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2024-10-20 23:42:52 -0700

    finish buffer perhaps

--- buffer.py
+++ buffer.py
@@ -1,5 +1,6 @@
 from typing import List, NamedTuple, Any
 import numpy as np
+import random
 
 
 class Frame(NamedTuple):
@@ -14,10 +15,13 @@ class ReplayBuffer:
         self.capacity = capacity
         self.buffer: List[Frame] = []
 
-    def add(self, frame: Frame):
+    def __len__(self):
+        return len(self.buffer)
+
+    def add(self, frame: Frame) -> None:
         self.buffer.append(frame)
         if len(self.buffer) > self.capacity:
             self.buffer.pop(0)
 
-    def sample(self, sz):
-
+    def sample(self, sz) -> List[Frame]:
+        return random.sample(self.buffer, sz)
